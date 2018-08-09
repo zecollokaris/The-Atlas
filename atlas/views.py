add459ba85b1f8a858efc339de749c2022b6cc73
zecollokaris <collo.kariss@gmail.com>
2018-08-09 23:47:23 +0300

    done with views doc strings

--- atlas/views.py
+++ atlas/views.py
@@ -2,8 +2,28 @@ from django.shortcuts import render
 
 from django.http import HttpResponse
 from django.urls import path
-from django.contrib.auth.decorators import login_required
+from . import models
+from django.contrib.auth.models import User
 
+from django.contrib.auth.decorators import login_required
+'''
+The @login_required declarator limits access of view function to only 
+authenticated users
+'''
+#---------------------------------------------------------------------#
+'''End Of Import'''
+#---------------------------------------------------------------------#
+
+
+
+#################################################################################################################################################################################
+#INDEX PAGE VIEW FUNCTION
+#################################################################################################################################################################################
+
+#Index page view function
 @login_required(login_url='/accounts/login/')
 def index(request):
     return render(request, 'display/home.html')
+
+#################################################################################################################################################################################
+#################################################################################################################################################################################
