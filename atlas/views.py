a8c082e8675f36eaad9719ff1bdc25c5f2350a02
zecollokaris <collo.kariss@gmail.com>
2018-08-09 16:41:21 +0300

    added import for requiring login

--- atlas/views.py
+++ atlas/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 
 from django.http import HttpResponse
 from django.urls import path
+from django.contrib.auth.decorators import login_required
 
 def index(request):
     return render(request, 'display/home.html')
