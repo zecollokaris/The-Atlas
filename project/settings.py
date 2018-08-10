b2f3e3d5dc023eb33e807bc20598c099d5f979e3
zecollokaris <collo.kariss@gmail.com>
2018-08-10 09:41:38 +0300

    done with login and registration

--- project/settings.py
+++ project/settings.py
@@ -122,3 +122,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
 STATIC_URL = '/static/'
+
+#enables static files to be found in root folder 
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static"),
+    
+]
+
+LOGIN_REDIRECT_URL = '/landing'
