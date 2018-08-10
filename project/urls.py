30fc27cfccebf96bddd10ac27ffb864016e4ba3d
zecollokaris <collo.kariss@gmail.com>
2018-08-10 13:20:01 +0300

    end of project

--- project/urls.py
+++ project/urls.py
@@ -5,7 +5,10 @@ from django.contrib import admin
 
 from atlas import views
 
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+
 from django.urls import include, path
+
 '''Here we have imported (include)-: this allows us to add the gram url pattern
 as done below this is to avoid filling up all our urls in this page'''
 
@@ -20,3 +23,5 @@ urlpatterns = [
     path('', include('atlas.urls')),
     path('admin/', admin.site.urls),
 ]
+
+urlpatterns += staticfiles_urlpatterns()
